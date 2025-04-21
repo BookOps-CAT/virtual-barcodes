681846b18bfe9e9f40568e01f3725629780746aa
klinga <klingaroo@gmail.com>
2025-04-21 13:06:14 -0400

    adds VirtualBarcode class and minter generator

---
+++ tests/test_barcodes.py
@@ -0,0 +1,62 @@
+from typing import Generator
+
+import pytest
+
+from virtual_barcodes.barcode import VirtualBarcode, minter
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        (1, 3),
+        ("1", 3),
+        ("00000001", 3),
+        (2, 1),
+        (12345678, 7),
+        (99999999, 3),
+        (23445571, 0),
+    ],
+)
+def test_calculate_digit_check(arg, expectation):
+    assert VirtualBarcode(prefix=73433, identifier=arg).digit_check == expectation
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        (1, "73433000000013"),
+        ("00000001", "73433000000013"),
+        (12345678, "73433123456787"),
+        (
+            99999999,
+            "73433999999993",
+        ),
+    ],
+)
+def test_VirtualBarcode_repr(arg, expectation):
+    assert repr(VirtualBarcode(prefix=73433, identifier=arg)) == expectation
+
+
+def test_VirtualBarcode_instance():
+    barcode = VirtualBarcode(prefix=73433, identifier=1)
+    assert barcode.prefix == 73433
+    assert barcode.zfill_identifier == "00000001"
+    assert barcode.digit_check == 3
+
+
+def test_minter_return_type():
+    assert isinstance(minter(1, 73433), Generator)
+
+
+def test_minter_return_value():
+    minted_barcodes = []
+    for barcode in minter(1, 5, 73433):
+        minted_barcodes.append(barcode)
+
+    assert minted_barcodes == [
+        "73433000000013",
+        "73433000000021",
+        "73433000000039",
+        "73433000000047",
+        "73433000000054",
+    ]
---
+++ virtual_barcodes/barcode.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+from typing import Generator
+
+
+class VirtualBarcode:
+    def __init__(self, identifier: int, prefix: int = 73433):
+        """
+        Initialize the VirtualBarcode instance.
+
+        Args:
+            prefix (int): The prefix for the barcodes.
+            identifier (int): The starting number for the barcodes without
+                a prefix or digit check.
+        """
+        self.prefix = prefix
+        self.zfill_identifier = str(identifier).zfill(8)
+        self.digit_check: int = self._calculate_digit_check()
+
+    def _calculate_digit_check(self) -> int:
+        """
+        Calculate the digit check for the barcode.
+        The digit check is the last digit of the barcode, calculated using a specific algorithm.
+        """
+        digits = [int(d) for d in f"{self.prefix}{self.zfill_identifier}"]
+        even_sum = sum(digits[i] for i in range(1, len(digits), 2))
+        odd_digits = [digits[i] for i in range(0, len(digits), 2)]
+
+        def get_odd_sum(i: int) -> int:
+            i_doubled = i * 2
+            if i_doubled > 9:
+                return 1 + (i_doubled % 10)
+            else:
+                return i_doubled
+
+        odd_sum = sum([get_odd_sum(i) for i in odd_digits])
+
+        return (10 - ((even_sum + odd_sum) % 10)) % 10
+
+    def __repr__(self):
+        return f"{self.prefix}{self.zfill_identifier}{self.digit_check}"
+
+
+def minter(
+    starting_sequence: int, batch_size: int, prefix: int = 73433
+) -> Generator[str, None, None]:
+    """
+    Mint a batch of 14-digit barcodes.
+
+    Args:
+        prefix (str): The prefix for the barcodes.
+        starting_sequence (int): The starting number for the barcodes.
+        batch_size (int): The number of barcodes to mint.
+
+    Returns:
+        list[str]: A list of minted barcodes.
+    """
+    for i in range(starting_sequence, starting_sequence + batch_size):
+        barcode = VirtualBarcode(identifier=i, prefix=73433)
+        yield str(barcode)
