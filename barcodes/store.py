14042a5a36df88ccb9d1d837225b800b6f0e882e
klinga <klingaroo@gmail.com>
2025-05-02 18:51:01 -0400

    adds calculate_range, prep_values, add_barcodes_to_worksheet, and add_barcodes_to_sheet

--- barcodes/store.py
+++ barcodes/store.py
@@ -1,7 +1,7 @@
 import os
 import json
 from pathlib import Path
-from typing import Optional, Any
+from typing import Optional, Any, Union
 
 import gspread
 
@@ -42,4 +42,62 @@ def create_worksheet(
     name: str, sheet: gspread.spreadsheet.Spreadsheet, rows: int
 ) -> gspread.worksheet.Worksheet:
     """Create a new worksheet."""
-    return sheet.add_worksheet(title=name, rows=rows, cols="5")
+    return sheet.add_worksheet(title=name, rows=rows, cols=5)
+
+
+def calculate_range(barcodes: list[str], worksheet: gspread.worksheet.Worksheet) -> str:
+    """Calculate the range for the worksheet."""
+    # Get the last row in the worksheet
+    last_row = len(worksheet.get_all_values()) + 1
+    # Calculate the range for the A and B columns
+    range_str = f"A{last_row}:B{last_row + len(barcodes)}"
+    return range_str
+
+
+def prep_values_for_main_ws_update(
+    barcodes: list[str], unit_code: str
+) -> list[tuple[str, str]]:
+    """Prepare data for batch update of the barcodes-to-date worksheet (main)."""
+    return [(b, unit_code) for b in barcodes]
+
+
+def prep_values_for_unit_ws_update(barcodes: list[str]) -> list[tuple[str]]:
+    """Prepare data for batch update of the unit worksheet."""
+    return [(b,) for b in barcodes]
+
+
+def add_barcodes_to_worksheet(
+    range_str: str,
+    values: Union[list[tuple[str, str]], list[tuple[str]]],
+    ws: gspread.worksheet.Worksheet,
+) -> None:
+    """Add a barcode to a given worksheet."""
+    ws.batch_update([{"range": range_str, "values": values}])
+
+
+def add_barcodes_to_sheet(
+    barcodes: list[str],
+    unit_code: str,
+    sheet_id: Optional[str] = "",
+) -> None:
+    """Add barcodes to the Google Sheet."""
+    if not sheet_id:
+        sheet_id = get_default_sheet()
+
+    sh = get_sheet(sheet_id)
+    # check if the unit worksheet exists and if not create it
+    try:
+        unit_ws = get_worksheet_by_name(unit_code, sh)
+    except gspread.WorksheetNotFound:
+        unit_ws = create_worksheet(unit_code, sh, len(barcodes))
+
+    # get barcodes-to-date worksheet (main)
+    main_ws = get_worksheet_by_name("barcodes-to-date", sh)
+    main_range_str = calculate_range(barcodes, main_ws)
+
+    main_values = prep_values_for_main_ws_update(barcodes, unit_code)
+    unit_range_str = calculate_range(barcodes, unit_ws)
+    unit_values = prep_values_for_unit_ws_update(barcodes)
+
+    add_barcodes_to_worksheet(main_range_str, main_values, main_ws)
+    add_barcodes_to_worksheet(unit_range_str, unit_values, unit_ws)
--- tests/test_store.py
+++ tests/test_store.py
@@ -4,7 +4,12 @@ import pytest
 import gspread
 
 
-from barcodes.store import get_sheet, get_worksheet_by_name
+from barcodes.store import (
+    get_sheet,
+    get_worksheet_by_name,
+    prep_values_for_main_ws_update,
+    prep_values_for_unit_ws_update,
+)
 
 
 @pytest.mark.webtest
@@ -23,3 +28,34 @@ def test_get_worksheet_by_name(test_sheet_id):
     assert isinstance(ws, gspread.worksheet.Worksheet)
     assert ws.title == "barcodes-to-date"
 
+
+@pytest.mark.webtest
+def test_get_worksheet_by_name_not_found(test_sheet_id):
+    with pytest.raises(gspread.WorksheetNotFound):
+        sh = get_sheet(test_sheet_id)
+        get_worksheet_by_name("non-existent-worksheet", sh)
+
+
+def test_prep_values_for_main_ws_update():
+    barcodes = ["123456789013", "123456789014"]
+    unit_code = "DEV"
+    assert prep_values_for_main_ws_update(barcodes, unit_code) == [
+        ("123456789013", "DEV"),
+        ("123456789014", "DEV"),
+    ]
+
+
+def test_prep_values_for_unit_ws_update():
+    barcodes = ["123456789013", "123456789014"]
+    assert prep_values_for_unit_ws_update(barcodes) == [
+        ("123456789013",),
+        ("123456789014",),
+    ]
+
+
+# def test_temp(test_sheet_id):
+#     # from barcodes.store import create_worksheet
+#     from barcodes.store import add_barcodes_to_sheet
+
+#     barcodes = ["123456789013", "123456789014"]
+#     add_barcodes_to_sheet(barcodes, "DEV", test_sheet_id)
