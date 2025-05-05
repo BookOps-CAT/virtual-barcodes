4d1bd09dab0912485c33a82a9f1c83b0628ea229
klinga <klingaroo@gmail.com>
2025-05-05 16:46:12 -0400

    adds test_sheet fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 from typing import Any
 
+import gspread
 import pytest
 
 
@@ -12,3 +13,12 @@ def test_sheet_id() -> Any:
     with open(fh) as f:
         data = json.load(f)
     return data["key"]
+
+
+@pytest.fixture
+def test_sheet(test_sheet_id) -> gspread.spreadsheet.Spreadsheet:
+    """Fixture to get the test Google Sheet."""
+    cred_fh = Path.home() / ".cred/.gsheet/google-sheet-virtual-barcodes.json"
+    gc = gspread.oauth(credentials_filename=cred_fh)
+    sh = gc.open_by_key(test_sheet_id)
+    return sh
