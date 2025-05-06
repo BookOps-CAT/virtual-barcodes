f95f0c435580a43e9aef17b93fd76d0d84beb069
klinga <klingaroo@gmail.com>
2025-05-06 12:25:41 -0400

    adds mint command

--- barcodes/cli.py
+++ barcodes/cli.py
@@ -1,5 +1,8 @@
 import click
 
+from barcodes.minter import mint_barcodes
+from barcodes.store import add_barcodes_to_sheet, determine_new_barcodes, get_sheet
+
 
 @click.group()
 def main_cli() -> None:
@@ -11,6 +14,24 @@ def main_cli() -> None:
 def test_cli() -> None:
     """Test CLI command"""
     click.echo("CLI is working!")
+
+
+@main_cli.command()
+@click.argument("unit_code", type=str, required=True)
+@click.argument("batch_size", type=int, required=True)
+def mint(unit_code: str, batch_size: int) -> None:
+    """
+    Mint barcodes for a given unit code and batch size.
+
+    Args:
+        unit_code (str): The unit/division's code using the barcodes.
+        batch_size (int): The number of barcodes to mint.
+    """
+    # Get the Google Sheet
+    sh = get_sheet()
+    barcodes = determine_new_barcodes(sh, batch_size)
+    add_barcodes_to_sheet(barcodes, unit_code, sh)
+    click.echo(f"Minted {len(barcodes)} barcodes for unit code {unit_code}.")
 
 
 def cli() -> None:
